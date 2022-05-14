ea76aa1d4aa89701be785ef811125e87547f164e
Alediran <alediran@gmail.com>
2022-05-14 02:11:28 -0700

    Implementing

---
+++ web/src/App/Store.tsx
@@ -0,0 +1,25 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query/react';
+import { api } from 'Services';
+
+const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(api.middleware)
+});
+
+setupListeners(store.dispatch);
+
+export default store;
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
