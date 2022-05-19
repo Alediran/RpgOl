9f3b80aefb22fd3e9a30a0557829e0e82b742673
Alediran <alediran@gmail.com>
2022-05-18 20:54:37 -0700

    Setting AutoMapper

---
+++ src/RpgOl.Application.Contracts/Boards/UpdateBoardDto.cs
@@ -0,0 +1,17 @@
+﻿using RpgOl.BoardCategories;
+using RpgOl.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace RpgOl.Boards
+{
+    public class UpdateBoardDto : EntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public BoardType Type { get; set; }
+        public GameSystem GameSystem { get; set; }
+        public List<BoardCategoryDto> BoardCategories { get; set; }
+    }
+}
