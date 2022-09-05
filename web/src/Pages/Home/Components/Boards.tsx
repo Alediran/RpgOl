e75fb3fc4c61b87647bff34928828b87300a68ef
Alediran <alediran@gmail.com>
2022-09-05 12:24:02 -0700

    Implement

--- web/src/Pages/Home/Components/Boards.tsx
+++ web/src/Pages/Home/Components/Boards.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/function-component-definition */
-import { useAppSelector } from "App/Hooks";
-import Localize from "Components/Localize/Index";
-import { Skeleton } from "primereact/skeleton";
 import React from "react";
+import { useAuth } from "react-oidc-context";
+import { Skeleton } from "primereact/skeleton";
 import { useGetAllBoardsQuery } from "Services/Boards";
 import { BoardTypes } from "Types/Enums";
+import Localize from "Components/Localize/Index";
 import BoardRow from "./BoardRow";
 
 const Boards: React.FC = () => {
   const {data: boards, isLoading: loadingBoards} = useGetAllBoardsQuery()
-  const { userId } = useAppSelector(state => state.session);
+  const { user } = useAuth()
+  console.log("User ID is ", user);
 
   return <div className='flex'>
     <div className="col-6">
@@ -17,13 +18,13 @@ const Boards: React.FC = () => {
         <b>{Localize.GamesYouOwn}</b>
       </div>
       <div className="container"> 
-        {loadingBoards ? <Skeleton width="100%" /> : boards?.filter(q => q.creatorId === userId && q.type === BoardTypes.Game).map((board) => <BoardRow key={board.id} id={board.id} name={board.name} />)}
+        {loadingBoards ? <Skeleton width="100%" /> : boards?.filter(q => q.creatorId === user?.profile.sub && q.type === BoardTypes.Game).map((board) => <BoardRow key={board.id} id={board.id} name={board.name} />)}
       </div>
       <div className="header">
         <b>{Localize.GamesYouPlay}</b>
       </div>
       <div className={loadingBoards ? '': 'container' }> 
-        {loadingBoards ? <Skeleton width="100%" /> : boards?.filter(q => q.creatorId !== userId && q.type === BoardTypes.Game).map((board) => <BoardRow key={board.id} id={board.id} name={board.name} />)}
+        {loadingBoards ? <Skeleton width="100%" /> : boards?.filter(q => q.creatorId !== user?.profile.sub && q.type === BoardTypes.Game).map((board) => <BoardRow key={board.id} id={board.id} name={board.name} />)}
       </div>
     </div>
     <div className="col-6">
