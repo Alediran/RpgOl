a4baed71048549b7dde8f7137129e11407b0b6f0
Alediran <alediran@gmail.com>
2023-11-04 00:27:27 -0700

    ABP Update

--- src/RpgOl.EntityFrameworkCore/EntityFrameworkCore/RpgOlDbContext.cs
+++ src/RpgOl.EntityFrameworkCore/EntityFrameworkCore/RpgOlDbContext.cs
@@ -24,26 +24,9 @@ namespace RpgOl.EntityFrameworkCore;
 [ReplaceDbContext(typeof(IIdentityDbContext))]
 [ReplaceDbContext(typeof(ITenantManagementDbContext))]
 [ConnectionStringName("Default")]
-public class RpgOlDbContext :
-    AbpDbContext<RpgOlDbContext>,
-    IRpgOlDbContext,
-    IIdentityDbContext,
-    ITenantManagementDbContext
+public class RpgOlDbContext : AbpDbContext<RpgOlDbContext>, IRpgOlDbContext, IIdentityDbContext, ITenantManagementDbContext
 {
-    /* Add DbSet properties for your Aggregate Roots / Entities here. */
-
     #region Entities from the modules
-
-    /* Notice: We only implemented IIdentityDbContext and ITenantManagementDbContext
-     * and replaced them for this DbContext. This allows you to perform JOIN
-     * queries for the entities of these modules over the repositories easily. You
-     * typically don't need that for other modules. But, if you need, you can
-     * implement the DbContext interface of the needed module and use ReplaceDbContext
-     * attribute just like IIdentityDbContext and ITenantManagementDbContext.
-     *
-     * More info: Replacing a DbContext of a module ensures that the related module
-     * uses this DbContext on runtime. Otherwise, it will use its own DbContext class.
-     */
 
     //Identity
     public DbSet<IdentityUser> Users { get; set; }
@@ -65,6 +48,8 @@ public class RpgOlDbContext :
     public DbSet<Group> Groups { get; set; }
     public DbSet<Post> Posts { get; set; }
     public DbSet<Character> Characters { get; set; }
+
+    public DbSet<IdentityUserDelegation> UserDelegations { get; set; }
 
     public RpgOlDbContext(DbContextOptions<RpgOlDbContext> options)
         : base(options)
